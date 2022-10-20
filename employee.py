dd2dff77da370011672f69c18cc5e35d03736ede
Umair402 <umair.qureshi@hotmail.co.uk>
2022-10-20 15:20:52 +0100

    add fullstops

--- employee.py
+++ employee.py
@@ -21,7 +21,7 @@ class BasicSalaryWorker(Employee):
         return self.salary
 
     def __str__(self):
-        return(f'{self.name} works on a monthly salary of {self.salary}. Their total pay is {self.get_pay()}')
+        return(f'{self.name} works on a monthly salary of {self.salary}. Their total pay is {self.get_pay()}.')
 
 
 
@@ -35,7 +35,7 @@ class BasicContractWorker(Employee):
         return self.rate * self.hours
 
     def __str__(self):
-        return(f'{self.name} works on a contract of {self.hours} hours at {self.rate}/hour. Their total pay is {self.get_pay()}')
+        return(f'{self.name} works on a contract of {self.hours} hours at {self.rate}/hour. Their total pay is {self.get_pay()}.')
 
 
 
@@ -50,7 +50,7 @@ class SalaryContractCommission(Employee):
         return self.salary + self.commContracts * self.commRate
 
     def __str__(self):
-        return(f'{self.name} works on a monthly salary of {self.salary} and receives a commission for {self.commContracts} contracts(s) at {self.commRate}/contract. Their total pay is {self.get_pay()}')
+        return(f'{self.name} works on a monthly salary of {self.salary} and receives a commission for {self.commContracts} contracts(s) at {self.commRate}/contract. Their total pay is {self.get_pay()}.')
 
 
 
@@ -66,7 +66,7 @@ class ContractContractCommission(Employee):
         return self.hours * self.rate + self.commContracts * self.commRate
 
     def __str__(self):
-        return(f'{self.name} works on a contract of {self.hours} hours at {self.rate}/hour and receives a commission for {self.commContracts} contracts(s) at {self.commRate}/contract. Their total pay is {self.get_pay()}')
+        return(f'{self.name} works on a contract of {self.hours} hours at {self.rate}/hour and receives a commission for {self.commContracts} contracts(s) at {self.commRate}/contract. Their total pay is {self.get_pay()}.')
 
 class SalaryBonusCommission(Employee):
     def __init__(self, name, salary, commBonus):
@@ -78,7 +78,7 @@ class SalaryBonusCommission(Employee):
         return self.salary + self.commBonus
 
     def __str__(self):
-        return(f'{self.name} works on a monthly salary of {self.salary} and receives a bonus commission of {self.commBonus}. Their total pay is {self.get_pay()}')
+        return(f'{self.name} works on a monthly salary of {self.salary} and receives a bonus commission of {self.commBonus}. Their total pay is {self.get_pay()}.')
 
 class ContractBonusCommission(Employee):
     def __init__(self, name, hours, rate, commBonus):
@@ -91,23 +91,35 @@ class ContractBonusCommission(Employee):
         return self.rate * self.hours + self.commBonus
 
     def __str__(self):
-        return(f'{self.name} works on a contract of {self.hours} hours at {self.rate}/hour and receives a bonus commission of {self.commBonus}. Their total pay is {self.get_pay()}')
+        return(f'{self.name} works on a contract of {self.hours} hours at {self.rate}/hour and receives a bonus commission of {self.commBonus}. Their total pay is {self.get_pay()}.')
 
 
 # Billie works on a monthly salary of 4000.  Their total pay is 4000.
 billie = BasicSalaryWorker('Billie',4000)
+print(billie.get_pay())
+print(billie.__str__())
 
 # Charlie works on a contract of 100 hours at 25/hour.  Their total pay is 2500.
 charlie = BasicContractWorker('Charlie', 100, 25)
+print(charlie.get_pay())
+print(charlie.__str__())
 
 # Renee works on a monthly salary of 3000 and receives a commission for 4 contract(s) at 200/contract.  Their total pay is 3800.
 renee = SalaryContractCommission('Renee', 3000, 4, 200)
+print(renee.get_pay())
+print(renee.__str__())
 
 # Jan works on a contract of 150 hours at 25/hour and receives a commission for 3 contract(s) at 220/contract.  Their total pay is 4410.
 jan = ContractContractCommission('Jan', 150, 25, 3, 220)
+print(jan.get_pay())
+print(jan.__str__())
 
 # Robbie works on a monthly salary of 2000 and receives a bonus commission of 1500.  Their total pay is 3500.
 robbie = SalaryBonusCommission('Robbie', 2000, 1500)
+print(robbie.get_pay())
+print(robbie.__str__())
 
 # Ariel works on a contract of 120 hours at 30/hour and receives a bonus commission of 600.  Their total pay is 4200.
 ariel = ContractBonusCommission('Ariel', 120, 30, 600)
+print(ariel.get_pay())
+print(ariel.__str__())
